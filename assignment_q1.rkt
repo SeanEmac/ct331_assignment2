dc059d1d740b89e3fd391d9c48947771a52f1b36
seanemac <S.mccann11@nuigalway.ie>
2018-10-30 13:43:28 +0000

    Q1

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,7 @@
 #lang racket
+(cons 1 2)
+(cons 1 (cons 2 (cons 3 empty)))
+(cons "string" (cons 9 (cons 1 (cons 2 (cons 3 empty)))))
+;(cons "string" (cons 9 '(1 2 3)))
+(list "string" 9 (list 1 2 3))
+(append '("string") '(9) '((1 2 3)))
